915eb58e303501bc6dd0d5a93bf5a78937825cdb
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-09-29 20:27:58 +0300

    add options and keymaps

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,42 @@
+-- Plugin maps are in their respective files
+
+-- Leader key for key combos
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "<A-h>", "<cmd>vertical resize -2<CR>", { desc = "Move split to left" })
+vim.keymap.set("n", "<A-l>", "<cmd>vertical resize +2<CR>", { desc = "Move split to left" })
+vim.keymap.set("n", "<A-j>", "<cmd>resize +2<CR>", { desc = "Move split to left" })
+vim.keymap.set("n", "<A-k>", "<cmd>resize -2<CR>", { desc = "Move split to left" })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,56 @@
-
+-- Line numbers
 vim.opt.number = true
+
+-- Relative line numbers
 vim.opt.relativenumber = true
+
+-- Enable mouse
+vim.opt.mouse = "a"
+
+-- Use system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Create new splits on right or below
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Make cursorline highlighted
+vim.opt.cursorline = true
+
+-- Make tabsize 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Turn tabs into spaces
+vim.opt.expandtab = true
+
+-- Enable better color support
+vim.opt.termguicolors = true
+
+-- Make searches case insensitive
+vim.opt.ignorecase = true
+
+-- If search has upper case, make it case sensitive
+vim.opt.smartcase = true
+
+-- Make longer lines to overflow instead of wrapping to nextline
+vim.opt.wrap = false
+
+-- Enable persistent undo, can undo to older changes than file opening
+vim.opt.undofile = true
+
+-- Enable folding based on syntax, might need plugin for this.
+vim.opt.foldmethod = "syntax"
+
+-- Make all folds are open by default when opened a file (Number is arbitrary, bigger than fold level)
+vim.opt.foldlevel = 99
+
+-- Disables mode display at bottom
+vim.opt.showmode = false
+
+-- Disables swap files
+vim.opt.swapfile = false
+
+-- Keeps cursor in the middle of screen (10 means at least 10 lines away from the borders)
+vim.opt.scrolloff = 999
