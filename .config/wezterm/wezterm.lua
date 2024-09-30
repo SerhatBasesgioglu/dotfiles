2d4b92bf0dde27ad8888c44c8a1ee144dad55b87
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-09-30 11:28:14 +0300

    change folder structure, change install script from symlink to copy

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,5 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.color_scheme = "Batman"
+return config
--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-#Git
-ln -sf ~/repos/dotfiles/git/ ~/.config
+read -p "This operation will override the current app configurations, do you want to continue? (y/n):" -n 1 -r
+echo
 
-#Neovim
-ln -sf ~/repos/dotfiles/nvim ~/.config/
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Operation cancelled"
+    exit 1
+fi
+
+echo "Defining config directories"
+DOTFILES_DIR=./.config/
+W11_CONFIG_DIR=/mnt/c/Users/serhat/.config/
+WSL_CONFIG_DIR=/home/serhat/.config/
+echo "Done"
+
+
+
+echo "Copying W11 configs..."
+cp -r "$DOTFILES_DIR/"* "$W11_CONFIG_DIR"
+echo "Done"
+
+
+echo "Copying WSL configs..."
+cp -r "$DOTFILES_DIR/"* "$WSL_CONFIG_DIR"
+echo "Done"
+
