3d5ffac839d0ac8c5290e0c32883f644b3511b43
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-12-07 19:46:37 +0300

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-DOTFILES_DIR=~/repos/dotfiles/
-W11_CONFIG_DIR=/mnt/c/Users/serhat/
-WSL_CONFIG_DIR=/home/serhat/
+DOTFILES_DIR=~/repos/dotfiles
+WSL_CONFIG_DIR=/home/serhat
 
 
 #Neovim
@@ -13,7 +12,6 @@ ln -sfn "$DOTFILES_DIR"/git "$WSL_CONFIG_DIR"/.config/
 
 #Wezterm
 ln -sfn "$DOTFILES_DIR"/wezterm "$WSL_CONFIG_DIR"/.config/
-cp -r "$DOTFILES_DIR"/wezterm "$W11_CONFIG_DIR"/.config/
 
 #Tmux
 ln -sfn "$DOTFILES_DIR"/tmux "$WSL_CONFIG_DIR"/.config/
