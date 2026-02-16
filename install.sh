bb1f64213463b007f0d88fe191f699371ace0363
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2026-02-16 19:44:55 +0300

    add kitty, update install script, add bashrc template

--- install.sh
+++ install.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 DOTFILES_DIR=~/repos/dotfiles
-WSL_CONFIG_DIR=/home/serhat
 
 
 #Neovim
-ln -sfn "$DOTFILES_DIR"/nvim "$WSL_CONFIG_DIR"/.config/
+ln -sfn "$DOTFILES_DIR"/nvim "$HOME"/.config/
 
 #Git
-ln -sfn "$DOTFILES_DIR"/git "$WSL_CONFIG_DIR"/.config/
-
-#Wezterm
-ln -sfn "$DOTFILES_DIR"/wezterm "$WSL_CONFIG_DIR"/.config/
+ln -sfn "$DOTFILES_DIR"/git "$HOME"/.config/
 
 #Tmux
-ln -sfn "$DOTFILES_DIR"/tmux "$WSL_CONFIG_DIR"/.config/
+ln -sfn "$DOTFILES_DIR"/tmux "$HOME"/.config/
 
 #Bash
-ln -sfn "$DOTFILES_DIR"/bash/bash_aliases "$WSL_CONFIG_DIR"/.bash_aliases
+ln -sfn "$DOTFILES_DIR"/bash/bash_aliases "$HOME"/.bash_aliases
+ln -sfn "$DOTFILES_DIR"/bash/.bashrc "$HOME"/.bashrc
 source ~/.bashrc
