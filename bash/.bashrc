a96243f4a92b8422ead68b79f4ee0b1a3693a2e0
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2026-02-21 20:40:41 +0300

    update

--- bash/.bashrc
+++ bash/.bashrc
@@ -10,8 +10,20 @@ alias grep='grep --color=auto'
 PS1='[\u@\h \W]\$ '
 
 # Enable colors
-force_color_prompt=yes
+color_prompt=yes
 
+if [ "$color_prompt" = yes ]; then
+    PS1='\[\e[1;32m\]\u@\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ '
+else
+    PS1='\u@\h:\w\$ '
+fi
+
+PS1="\[\e]0;\u@\h: \w\a\]$PS1"
 
 
 source ~/.bash_aliases
+
+
+dev() {
+  "$HOME/repos/devcontainer/run.sh" "$@"
+}
--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-DOTFILES_DIR=~/repos/dotfiles
+DOTFILES_DIR="${DOTFILES_DIR:-$HOME/repos/dotfiles}"
 
+mkdir -p ~/.config
 
-#Neovim
-ln -sfn "$DOTFILES_DIR"/nvim "$HOME"/.config/
+for dir in nvim git tmux; do
+  ln -sfn "$DOTFILES_DIR/$dir" "$HOME/.config/$dir"
+done
 
-#Git
-ln -sfn "$DOTFILES_DIR"/git "$HOME"/.config/
-
-#Tmux
-ln -sfn "$DOTFILES_DIR"/tmux "$HOME"/.config/
 
 #Bash
 ln -sfn "$DOTFILES_DIR"/bash/bash_aliases "$HOME"/.bash_aliases
